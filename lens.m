df2912ba798396273a86d47ca23b94f975f1534f
qq331965526 <yuzhuwang2017@u.northwestern.edu>
2016-03-10 00:03:31 -0600

    Added files via upload

--- lens.m
+++ lens.m
@@ -1,22 +1,14 @@
-%only works when there is no extremly bright part in the picture, which
-%means indoors and no light in the photo
-
 function img_out=lens(img_in);
 img_out=img_in;
+HSV=rgb2hsv(img_in);
+v=HSV(:,:,3);
+vm=mean(v(:));
+if vm<0.4
+    x=0.4/vm;
+HSV(:,:,3)=HSV(:,:,3)*x;
+HSV(:,:,3)=histeq(HSV(:,:,3));
 
-r=img_in(:,:,1);
-g=img_in(:,:,2);
-b=img_in(:,:,3);
-rm=mean(r(:));
-gm=mean(g(:));
-bm=mean(b(:));
-Y = 0.299*rm + 0.587*gm + 0.114*bm;
-if Y<100
- x=100/Y;
-img_out(:,:,1)=img_in(:,:,1)*x;
-img_out(:,:,2)=img_in(:,:,2)*x;
-img_out(:,:,3)=img_in(:,:,3)*x;
-
+img_out=hsv2rgb(HSV);
 figure,imshow(img_out);
 title('after lens correction')
 
